eea9adca19ebfd2de100ba456ef36fce01b07874
ioannup <ioannup@gmail.com>
2017-04-23 18:54:00 +0000

    pretty-plugins.php edited online with Bitbucket

--- pretty-plugins.php
+++ pretty-plugins.php
@@ -110,16 +110,6 @@ class WMD_PrettyPlugins extends WMD_PrettyPlugins_Functions {
                     switch_to_blog( 1 );
                     $wp_upload_dir = wp_upload_dir();
                     restore_current_blog();
-
-			foreach ($wp_upload_dir as $type => $value)
-				if($type == 'basedir' || $type == 'baseurl') {
-					$parts = explode('/', $value);
-					if(is_numeric(end($parts))) {
-						array_pop($parts);
-						array_pop($parts);
-						$wp_upload_dir[$type] = implode('/', $parts);
-					}
-				}
                 }
 		$this->plugin_dir_custom = $wp_upload_dir['basedir'].'/prettyplugins/';
 		$this->plugin_dir_url_custom = $wp_upload_dir['baseurl'].'/prettyplugins/';
